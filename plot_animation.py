31c564302567ef81cbab3f6ba5520e36c561e5fa
Gustavo Souza <gdssouza@id.uff.br>
2020-12-26 22:50:43 -0300

    Adicionando barra de cores

--- plot_animation.py
+++ plot_animation.py
@@ -11,6 +11,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation, PillowWriter
 import cartopy.crs as ccrs
+
+anos = np.arange(1960, 2021, 1)
 projection = ccrs.Orthographic(central_latitude=90.0)
 
 # lendo dados
@@ -24,13 +26,17 @@ ax.gridlines()
 def animation_frame(ano):
     df = data[data.Date == ano]
     ax.set_title(ano)
-    return ax.scatter(df.Lon, df.Lat, c=df.Bt,\
-                          transform = ccrs.PlateCarree(),\
-                          cmap = plt.cm.RdBu_r)
+    scat = ax.scatter(df.Lon, df.Lat, c=df.Bt,\
+                  transform = ccrs.PlateCarree(),\
+                  cmap = plt.cm.RdBu_r)
+    #print("Plotando IGRF de",ano)
+    return scat
+
+fig.colorbar(animation_frame(anos[0]),
+             label='Totalintensity in nanoTesla (nT)')
 
 # gerando animação
-anos = np.arange(1960, 2021, 1)
-animation = FuncAnimation(fig, func=animation_frame, frames=anos, interval=10)
-
+animation = FuncAnimation(fig, func=animation_frame, frames=anos)
+        
 print("Salvando ...")
-animation.save('animation.gif', writer=PillowWriter(fps=40))
+animation.save('animation.gif', writer=PillowWriter(fps=5))
